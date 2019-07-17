ed33a6e7af40b2aff492700f6a994e88d2441b89
J-G Demathieu <jdemathieu@gmail.com>
2019-07-16 17:44:16 -0700

    Add eventAPIClient

--- src/predict.py
+++ src/predict.py
@@ -1,26 +1,27 @@
 from db import DB
+import EventAPIClient
+
 
 class Logic():
-  def __init__(self):
-    self.db = DB()
-    self.event_model, self.user_model, self.event_df = self.db.load_models()
+    def __init__(self):
+        self.db = DB()
+        self.event_model, self.user_model, self.event_df = self.db.load_models()
 
-  def predict(self, json_event):
-    '''
+    def predict(self, event_dict):
+        '''
     INPUT:
       one event
 
     OUTPUT:
       risk score from 0: safe to 1: sure fraud
     '''
-    # TODO: vectorize the event per event_model
+        # TODO: vectorize the event per event_model
 
-    # predict 
+        # predict
 
-    # TODO: add the data + score as 1 row in the event_df
+        # TODO: add the data + score as 1 row in the event_df
 
-    return 0.5  # Basic model!
+        return 0.5  # Basic model!
 
-  def save(self):
-    self.db.save()
-  
+    def save(self):
+        self.db.save()
